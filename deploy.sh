0612c7892b83baa8f51e04e7358d200f5216d329
Travis CI User <travis@example.org>
2017-09-26 22:33:57 +0000

    publish: blargh
    generated from commit e5f0e818d7f94d644c7c363e54f192c95d6eb86e

--- deploy.sh
+++ deploy.sh
@@ -76,6 +76,7 @@ build_master () {
   git push SSH gh-pages
 }
 
+git remote add SSH git@github.com:hueneburg/ultimate-comparison-BASE.git
 git fetch --all
 # decide which functions should be called
 if [[ ${TRAVIS_PULL_REQUEST} != false ]]; then
